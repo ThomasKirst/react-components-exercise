04ad43426d2af1a65c0ea1724a6a855dca51e01f
Anita Schiller <anita.s1@gmx.de>
2021-12-22 08:33:31 +0100

    Built basic structure

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+/* ---- IMPORTS ---- */
+import { useState } from 'react';
+
+function App() {
+  /* ---- NORMALES JS ---- */
+  const recipesArchive = [
+    {
+      title: 'Pizza',
+      difficulty: 'medium',
+      duration: 30,
+    },
+    {
+      title: 'Ingwer Smoothie',
+      difficulty: 'low',
+      duration: 5,
+    },
+    {
+      title: 'Zucchini Auflauf',
+      difficulty: 'high',
+      duration: 50,
+    },
+  ];
+
+  const [cookingTimer, setCookingTimer] = useState(0);
+
+  /*   ---- JSX-Teil ---- */
+  /* Alles, was nicht aussieht wie HTML oder Textinhalt ist, muss in geschweifte Klammern! "Fenster zu normalem JS" */
+  return (
+    <div>
+      {/* HEADER */}
+      <header>
+        <h1>Recipes for React</h1>
+        <p>Time until your chosen meal is ready: {cookingTimer}</p>
+      </header>
+      <main>
+        {/*  REZEPTKARTEN */}
+
+        {recipesArchive.map((recipe, index) => (
+          <article key={index}>
+            <h2>{recipe.title}</h2>
+            <p>
+              Difficulty is {recipe.difficulty} and takes {recipe.duration}{' '}
+              minutes to make.
+            </p>
+            <button onClick={() => setCookingTimer(recipe.duration)}>
+              Set timer
+            </button>
+          </article>
+        ))}
+      </main>
+      {/* FOOTER */}
+      <footer>
+        <nav>
+          <a href="/">Home</a>{' '}
+          {/* Dummy Links - sollten in React Router durch NavLinks ersetzt werden */}
+          <a href="/click">Page 1</a>
+          <a href="/characters">Page 2</a>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+/* ---- STYLED-COMPONENTS ---- */
+
+export default App;
